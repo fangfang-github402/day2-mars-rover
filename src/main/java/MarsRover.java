a827bf598f086e8c272c96ef0d20ce1b09362f14
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-19 21:48:23 +0800

    feat:handle batch commands

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -14,21 +14,25 @@ public class MarsRover {
         return this.coordinate.X() + ":" + this.coordinate.Y() + ":" + this.direction;
     }
 
-    public String executeCommand(String command) {
-        if(command.equals("L")){
-            orientation = orientation.turnLeft();
-            direction = orientation.getDirection();
-        }
-        if(command.equals("R")){
-            orientation = orientation.turnRight();
-            direction = orientation.getDirection();
-        }
-        if(command.equals("M")){
-            coordinate = orientation.moveForward(coordinate);
-        }
-        if(command.equals("B")){
-            coordinate = orientation.moveBackward(coordinate);
-        }
+    public String executeCommand(String commands) {
+        commands.chars()
+                .mapToObj(c -> (char) c)
+                .forEach(command -> {
+                    if(command.equals('L')) {
+                        orientation = orientation.turnLeft();
+                        direction = orientation.getDirection();
+                    }
+                    if(command.equals('R')) {
+                        orientation = orientation.turnRight();
+                        direction = orientation.getDirection();
+                    }
+                    if(command.equals('M')) {
+                        coordinate = orientation.moveForward(coordinate);
+                    }
+                    if(command.equals('B')) {
+                        coordinate = orientation.moveBackward(coordinate);
+                    }
+                });
         return showStatus();
     }
 }
