7bee16b8749614d18f5c85da3052d0a268f7811c
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-11-19 21:13:35 +0800

    style:add comments

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -15,101 +15,135 @@ public class MarsRoverTest {
 
     @Test
     public void should_face_west_when_orient_north_and_turn_left(){
+        //Given
         MarsRover rover = new MarsRover();
+        //When
         String report = rover.executeCommand("L");
+        //Then
         assertEquals("0:0:W",report);
     }
 
     @Test
     public void should_face_south_when_orient_west_and_turn_left(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("L");
+        //Then
         assertEquals("0:0:S",report);
     }
 
     @Test
     public void should_face_east_when_orient_south_and_turn_left(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("L");
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("L");
+        //Then
         assertEquals("0:0:E",report);
     }
 
     @Test
     public void should_face_north_when_orient_east_and_turn_left(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("L");
         rover.executeCommand("L");
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("L");
+        //Then
         assertEquals("0:0:N",report);
     }
 
     @Test
     public void should_face_east_when_orient_north_and_turn_right(){
+        //Given
         MarsRover rover = new MarsRover();
+        //When
         String report = rover.executeCommand("R");
+        //Then
         assertEquals("0:0:E",report);
     }
 
     @Test
     public void should_face_south_when_orient_east_and_turn_right(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("R");
+        //When
         String report = rover.executeCommand("R");
+        //Then
         assertEquals("0:0:S",report);
     }
 
     @Test
     public void should_face_west_when_orient_south_and_turn_right(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("R");
         rover.executeCommand("R");
+        //When
         String report = rover.executeCommand("R");
+        //Then
         assertEquals("0:0:W",report);
     }
 
     @Test
     public void should_face_north_when_orient_west_and_turn_right(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("R");
         rover.executeCommand("R");
         rover.executeCommand("R");
+        //When
         String report = rover.executeCommand("R");
+        //Then
         assertEquals("0:0:N",report);
     }
 
     @Test
     public void should_incress_y_when_orient_north_and_move(){
+        //Given
         MarsRover rover = new MarsRover();
+        //When
         String report = rover.executeCommand("M");
+        //Then
         assertEquals("0:1:N",report);
     }
 
     @Test
     public void should_descress_x_when_orient_west_and_move(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("M");
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("M");
+        //Then
         assertEquals("-1:1:W",report);
     }
 
     @Test
     public void should_decress_y_when_orient_south_and_move(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("M");
         rover.executeCommand("L");
         rover.executeCommand("M");
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("M");
+        //Then
         assertEquals("-1:0:S",report);
     }
 
     @Test
     public void should_increase_x_when_orient_east_and_move(){
+        //Given
         MarsRover rover = new MarsRover();
         rover.executeCommand("M");
         rover.executeCommand("L");
@@ -117,7 +151,9 @@ public class MarsRoverTest {
         rover.executeCommand("L");
         rover.executeCommand("M");
         rover.executeCommand("L");
+        //When
         String report = rover.executeCommand("M");
+        //Then
         assertEquals("0:0:E",report);
     }
 }
